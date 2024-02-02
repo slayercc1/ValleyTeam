fc8a1c6d62db8c8cd88145846a1b58590e10c5d2
Slayer <108528746+slayercc1@users.noreply.github.com>
2024-02-02 17:38:44 +0300

    Update ValleyTeam.php

--- ValleyTeam.php
+++ ValleyTeam.php
@@ -1,1 +1,41 @@
+<!-- S L A Y E R --> 
+<html>
+   <head>
+      <title>Hacked By Slayer</title>
+      <meta charset="UTF-8">
+	  <link rel="shortcut icon" href="https://i.hizliresim.com/h8g536m.png">
+      <meta name="theme-color" content="#000">
+      <meta name="description" content="just a memory after i'm gone">
+      <meta property="og:description" content="just a memory after i'm gone">
+	  <meta property="og:image" content="https://i.hizliresim.com/8qxvtjs.gif">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+      <link rel="stylesheet" href="https://bit.ly/2UGCIC5">
 
+      <style type='text/css'>body,a:hover{cursor:url(https://cur.cursors-4u.net/symbols/sym-1/sym46.cur),progress!important;}</style>
+   </head>
+   <body bgcolor="black" text="white" oncontextmenu="return!1" onkeydown="return!1" onmousedown="return!1" onclick='document.getElementById("Slayer").play(),fs()' id="body" onload="typeWriter()">
+      <style type="text/css">center{font-family:Courier}img{opacity:80%}red{color:red}#background-video{height:100vh;width:100vw;object-fit:cover;position:fixed;left:0;right:0;top:0;bottom:0;z-index:-1}font{text-shadow:#000 0 0 3px;-webkit-font-smoothing:antialiased}div{animation:glitch 1s linear infinite}@keyframes glitch{2%,64%{transform:translate(2px,0) skew(0)}4%,60%{transform:translate(-2px,0) skew(0)}62%{transform:translate(0,0) skew(5deg)}}div:after,div:before{content:attr(title);position:absolute;left:0}div:before{animation:glitchTop 1s linear infinite;clip-path:polygon(0 0,100% 0,100% 33%,0 33%);-webkit-clip-path:polygon(0 0,100% 0,100% 33%,0 33%)}@keyframes glitchTop{2%,64%{transform:translate(2px,-2px)}4%,60%{transform:translate(-2px,2px)}62%{transform:translate(13px,-1px) skew(-13deg)}}div:after{animation:glitchBotom 1.5s linear infinite;clip-path:polygon(0 67%,100% 67%,100% 100%,0 100%);-webkit-clip-path:polygon(0 67%,100% 67%,100% 100%,0 100%)}@keyframes glitchBotom{2%,64%{transform:translate(-2px,0)}4%,60%{transform:translate(-2px,0)}62%{transform:translate(-22px,5px) skew(21deg)}}</style>
+      <script language="JavaScript">function confirmExit(){return}function fs(){var e=document.documentElement;e.requestFullscreen?e.requestFullscreen():e.msRequestFullscreen?e.msRequestFullscreen():e.mozRequestFullScreen?e.mozRequestFullScreen():e.webkitRequestFullscreen&&e.webkitRequestFullscreen(),document.getElementById("body").style.cursor="none",document.onkeydown=function(e){return!1},document.addEventListener("keydown",e=>{"F11"==e.key&&e.preventDefault()})}window.onbeforeunload=confirmExit;</script><script id="rendered-js">document.addEventListener("DOMContentLoaded",function(e){var n=[" ! your security get down !","Remember ! We are Türkic !"];!function e(t){void 0===n[t]&&setTimeout(function(){e(0)},3e4),t<n[t].length&&function e(t,n,o){n<t.length?(document.getElementById("hekerabies").innerHTML=t.substring(0,n+1),setTimeout(function(){e(t,n+1,o)},150)):"function"==typeof o&&setTimeout(o,7e3)}(n[t],0,function(){e(t+1)})}(0)})</script>
+      <audio src="https://cdn.discordapp.com/attachments/1155480892651872306/1167439588558639145/slayer.mp4" autoplay="true" id="Slayer" loop=""></audio>
+      <video id="background-video" src="https://cdn.discordapp.com/attachments/1155480892651872306/1167440378656456735/slayer1.mp4" autoplay="" loop="" muted="" style="position:fixed;object-fit:cover" poster="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=">
+         <source src="https://cdn.discordapp.com/attachments/1155480892651872306/1167439588558639145/slayer.mp4" type="video/webm">
+      </video>
+      <table width="100%" height="80%">
+         <td>
+            <center>
+           <img src="https://cdn.discordapp.com/attachments/1155480892651872306/1202936617602514944/tr3.png" width="260" height="230">
+			<center><br><br><font size="5"></font><br>Hacked By Slayer<br><font>
+			<center><font size="5"></font><br>-- VALLEY COMMUNITY --<br><font>
+            
+			<br> <br>
+            
+			   <center><font size="4"><b>Slayer - ZaRziRdA - KanNA - Keyn - Ruffluz - Tedky - Leafter - Aytlogo_Luxer - Crispusatyrus - ShadowGhostZ</b> </font></center>
+               <center><font size="4"><b>VegaS - Overdose - Angarali Joo - Wogira - 4KR3P - ALFONZO - Alt4ir - TurKLoJeN - ACE Veen - Muffin - Nattaki - Dao026</b> </font></center>
+            
+         </font></center></font></center></center></td>
+      </table>
+      <script data-cfasync="false" src="https://cdnjs.cloudflare.com/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+	  	  <center><div id="footer"><br>
+	      <a href="https://t.me/slayercc" target="_blank" style="color: white;">Telegram</a>
+		  </div></center>
+		  </body></html>
